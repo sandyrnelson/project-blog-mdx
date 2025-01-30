72510804e8cb20c95b1a44b9f9310c5392b20546
Sandy Nelson <sandy.nelson@nexius.hu>
2025-01-30 11:18:12 +0100

    add dynamic loading of blog posts

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,35 @@
 import React from 'react';
 
-import BlogSummaryCard from '@/components/BlogSummaryCard';
+import BlogSummaryCard from '../components/BlogSummaryCard';
+import Spinner from '../components/Spinner';
 
 import styles from './homepage.module.css';
+import { getBlogPostList } from '../helpers/file-helpers';
 
 function Home() {
-  return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.mainHeading}>
-        Latest Content:
-      </h1>
+	return (
+		<div className={styles.wrapper}>
+			<h1 className={styles.mainHeading}>Latest Content:</h1>
+			<React.Suspense fallback={<Spinner />}>
+				<Blogs />
+			</React.Suspense>
+		</div>
+	);
+}
 
-      {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
-    </div>
-  );
+async function Blogs() {
+	const blogs = await getBlogPostList();
+	console.log(blogs);
+
+	return blogs.map((blog) => (
+		<BlogSummaryCard
+			key={blog.slug}
+			slug={blog.slug}
+			title={blog.title}
+			abstract={blog.abstract}
+			publishedOn={blog.publishedOn}
+		/>
+	));
 }
 
 export default Home;
