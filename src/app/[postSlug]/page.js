1497c6ed70e990b3b3e179eb00217a532c804dae
Sandy Nelson <sandy.nelson@nexius.hu>
2025-01-30 12:01:18 +0100

    modify blog post to load markdown formatted

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,26 +1,26 @@
 import React from 'react';
-
+import { MDXRemote } from 'next-mdx-remote/rsc';
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
+import { loadBlogPost } from '@/helpers/file-helpers';
 
-function BlogPost() {
-  return (
-    <article className={styles.wrapper}>
-      <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
-      />
-      <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
-      </div>
-    </article>
-  );
+
+async function BlogPost({ params }) {
+	const { postSlug } = await params;
+	const blog = await loadBlogPost(postSlug);
+
+	return (
+		<article className={styles.wrapper}>
+			<BlogHero
+				title={blog.frontmatter.title}
+				publishedOn={blog.frontmatter.publishedOn}
+			/>
+			<div className={styles.page}>
+				<MDXRemote source={blog.content} />
+			</div>
+		</article>
+	);
 }
 
 export default BlogPost;
